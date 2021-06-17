9f89e30ddd7e9d625d77d6e45f927ea326db3045
Dong Du <dd_nirvana@sjtu.edu.cn>
2021-06-17 22:47:41 +0800

    monitor: add notes in sm.h
    Signed-off-by: Dong Du <dd_nirvana@sjtu.edu.cn>

--- opensbi-0.9/include/sm/sm.h
+++ opensbi-0.9/include/sm/sm.h
@@ -12,6 +12,11 @@
 #include <sm/enclave_args.h>
 #include <sm/ipi.h>
 
+
+/*
+ * Note: the hard-coded SM base and size depends on the M-mode firmware,
+ * 	 e.g., in OpenSBI, you should check the firmware range in platform/generic/config.mk
+ * */
 #define SM_BASE 0x80000000
 #define SM_SIZE 0x200000
 
