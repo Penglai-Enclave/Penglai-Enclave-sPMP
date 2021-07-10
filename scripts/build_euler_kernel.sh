8ff07757c57b99f0c84e448239ab68286786e68f
Dong Du <dd_nirvana@sjtu.edu.cn>
2021-07-10 12:56:34 +0800

    scripts: update scripts to build openEuler kernel from config
    Signed-off-by: Dong Du <dd_nirvana@sjtu.edu.cn>

--- scripts/build_euler_kernel.sh
+++ scripts/build_euler_kernel.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+## Author: Dong Du
+## This scripts should run in the Penglai-Build container
+
+## To create openEuler kernel config
+cd /home/penglai/penglai-enclave/openeuler-kernel
+cat openEuler_riscv64_defconfig >> arch/riscv/configs/defconfig
+make -s mrproper
+CROSS_COMPILE=riscv64-unknown-linux-gnu- make ARCH=riscv defconfig
+
+
+## To build kernel and enclave-driver
 cd /home/penglai/penglai-enclave/openeuler-kernel
 CROSS_COMPILE=riscv64-unknown-linux-gnu- make ARCH=riscv -j8
 cd /home/penglai/penglai-enclave/penglai-enclave-driver
