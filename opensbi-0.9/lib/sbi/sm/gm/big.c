94c5166cbf66b6eea9315ebaf705f2efb5800944
fengeh <2748250768@qq.com>
2021-10-13 09:46:44 +0800

    add source of gm algorithm
    Signed-off-by: fengeh <2748250768@qq.com>

--- opensbi-0.9/lib/sbi/sm/gm/big.c
+++ opensbi-0.9/lib/sbi/sm/gm/big.c
@@ -1,3 +1,8 @@
+/************************************************************************
+ * gm algorithms used by Penglai Monitor
+ * This file is copied from https://github.com/Penglai-Enclave/gm.git
+ ************************************************************************/
+
 #include "sm/gm/big.h"
 
 typedef struct
