b9f132945eaa7a552eab638dab11efb596fe1dd6
Dong Du <dd_nirvana@sjtu.edu.cn>
2022-07-01 00:19:33 +0800

    pmptable test: add microbench to simulate basic ops and their costs
    Signed-off-by: Dong Du <dd_nirvana@sjtu.edu.cn>

--- opensbi-0.9/lib/sbi/sm/pmptable.c
+++ opensbi-0.9/lib/sbi/sm/pmptable.c
@@ -14,6 +14,15 @@
 static uintptr_t pmpt1_root[RISCV_PGSIZE / sizeof(uintptr_t)] __attribute__((aligned(RISCV_PGSIZE)));
 static uintptr_t pmpt1_leaf[RISCV_PGSIZE / sizeof(uintptr_t)][RISCV_PGSIZE / sizeof(uintptr_t)] __attribute__((aligned(RISCV_PGSIZE)));
 
+static u64 dd_get_ticks(void)
+{
+	unsigned long n;
+	__asm__ __volatile__("rdtime %0" : "=r"(n));
+	return n;
+}
+
+void pmpt_microbench(void);
+
 /* pmpt2 for: [0x480000000,0x880000000]*/
 //uintptr_t pmpt2_root[RISCV_PGSIZE / sizeof(uintptr_t)] __attribute__((aligned(RISCV_PGSIZE)));
 //uintptr_t pmpt2_leaf[RISCV_PGSIZE / sizeof(uintptr_t)][RISCV_PGSIZE / sizeof(uintptr_t)] __attribute__((aligned(RISCV_PGSIZE)));
@@ -21,6 +30,8 @@ static uintptr_t pmpt1_leaf[RISCV_PGSIZE / sizeof(uintptr_t)][RISCV_PGSIZE / siz
 void init_pmpt(){
 	int count = RISCV_PGSIZE / sizeof(uintptr_t);
 	int i,j;
+
+	pmpt_microbench();
 
 	printm("[Dd@%s] count == %d\n", __func__, count);
 
@@ -87,4 +98,198 @@ void init_pmpt(){
 	//set_pmp_reg(4, &lower_bound, &invalid_cfg);
 	//set_pmp_reg(5, &upper_bound, &pmptable_cfg);
 	//set_pmp_reg(6, &root_pt_addr, &invalid_cfg);
+}
+
+void pmp_switch_test(){
+  	struct pmp_config_t pmp_config;
+  	pmp_config.paddr = 0xdddd;
+  	pmp_config.size = 0x4UL;
+  	pmp_config.mode = PMP_OFF;
+  	pmp_config.perm = PMP_R | PMP_W | PMP_X;
+  	set_pmp(2, pmp_config);
+  	set_pmp(3, pmp_config);
+  	set_pmp(4, pmp_config);
+  	set_pmp(5, pmp_config);
+  	set_pmp(6, pmp_config);
+  	set_pmp(7, pmp_config);
+}
+
+void pmpt_switch_test(){
+  	struct pmp_config_t pmp_config;
+  	pmp_config.paddr = 0xdddd; //assuming this as the pmptable addr
+  	pmp_config.size = 0x4UL;
+  	pmp_config.mode = PMP_OFF;
+  	pmp_config.perm = PMP_R | PMP_W | PMP_X | PMP_T;
+  	set_pmp(2, pmp_config);
+  	set_pmp(3, pmp_config);
+  	set_pmp(4, pmp_config);
+  	set_pmp(5, pmp_config);
+  	set_pmp(6, pmp_config);
+  	set_pmp(7, pmp_config);
+}
+
+void pmp_alloc_region(){
+  	struct pmp_config_t pmp_config;
+  	pmp_config.paddr = 0x1000;
+  	pmp_config.size = 0x100UL;
+  	pmp_config.mode = PMP_A_NAPOT;
+  	pmp_config.perm = PMP_R | PMP_W | PMP_X;
+  	set_pmp_and_sync(2, pmp_config);
+}
+
+void pmp_free_region(){
+  	struct pmp_config_t pmp_config;
+  	pmp_config.paddr = 0x0;
+  	pmp_config.size = 0x0UL;
+  	pmp_config.mode = PMP_OFF;
+  	pmp_config.perm = PMP_R | PMP_W | PMP_X;
+  	set_pmp_and_sync(2, pmp_config);
+}
+
+/*
+ * is_seg:
+ * 	0: update an HPMP_Table only
+ * 	1: update both seg and HPMP_Table
+ * size:
+ * 	the new region's size (unit is KB)
+ * */
+void pmpt_alloc_region(int is_seg, unsigned long size){
+  	struct pmp_config_t pmp_config;
+	// we should update the pmptable all the time for both seg and table choice
+	int granule = 64;
+	if (size>32768)
+		granule = 327688;
+	for (int i=0; i<size; i+=granule){ //each write can update 64KB pages (or <64KB pages)
+		pmpt1_leaf[0][(i/granule)%512] = 0xffffffffffffffff;
+		if (size-i<32768) granule = 64;
+	}
+	if (is_seg) {
+		// in case of seg, we should also update a seg register
+  		pmp_config.paddr = 0x1000;
+  		pmp_config.size = size;
+  		pmp_config.mode = PMP_A_NAPOT;
+  		pmp_config.perm = PMP_R | PMP_W | PMP_X;
+  		set_pmp_and_sync(2, pmp_config);
+	}else{
+  		set_pmp_and_sync(3, pmp_config);
+	}
+
+  	//pmp_config.paddr = 0x1000;
+  	//pmp_config.size = size*1024;
+  	//pmp_config.mode = PMP_NAPOT;
+  	//pmp_config.perm = PMP_R | PMP_W | PMP_X | PMP_T;
+}
+
+void pmpt_free_region(int is_seg, unsigned long size){
+  	struct pmp_config_t pmp_config;
+	// we should update the pmptable all the time for both seg and table choice
+	for (int i=0; i<size; i+=64) //each write can update 64KB pages (or <64KB pages)
+		pmpt1_leaf[0][(i/64)%512] = 0x0;
+
+	if (is_seg) {
+		// in case of seg, we should also update a seg register
+  		pmp_config.paddr = 0x1000;
+  		pmp_config.size = size;
+  		pmp_config.mode = PMP_OFF;
+  		pmp_config.perm = PMP_R | PMP_W | PMP_X;
+		//this will also sync TLBs with pmptables
+  		set_pmp_and_sync(2, pmp_config);
+	}else{
+	  	set_pmp_and_sync(3, pmp_config);
+	}
+}
+
+
+
+void pmpt_microbench(void){
+	u64 begin_tick, end_tick, exe_ticks[100], avg_ticks;
+	avg_ticks = 0;
+	printm("[PMPT-Micro#1 PMP-switch (unit: cycles)]: ");
+	for (int i=0; i<10; i++){
+		begin_tick = dd_get_ticks();
+		pmp_switch_test();
+		end_tick = dd_get_ticks();
+		exe_ticks[i] = end_tick - begin_tick;
+		avg_ticks += exe_ticks[i];
+		printm("%lu,", exe_ticks[i]);
+	}
+	printm("\n avg: %lu cycles\n", avg_ticks/10);
+
+	avg_ticks = 0;
+	printm("[PMPT-Micro#2 PMPT-switch (unit: cycles)]: ");
+	for (int i=0; i<10; i++){
+		begin_tick = dd_get_ticks();
+		pmpt_switch_test();
+		end_tick = dd_get_ticks();
+		exe_ticks[i] = end_tick - begin_tick;
+		avg_ticks += exe_ticks[i];
+		printm("%lu,", exe_ticks[i]);
+	}
+	printm("\n avg: %lu cycles\n", avg_ticks/10);
+
+	avg_ticks = 0;
+	printm("[PMPT-Micro#3 PMP Alloc (unit: cycles)]: ");
+	for (int i=0; i<16; i++){
+		begin_tick = dd_get_ticks();
+		pmp_alloc_region();
+		end_tick = dd_get_ticks();
+		exe_ticks[i] = end_tick - begin_tick;
+		avg_ticks += exe_ticks[i];
+		printm("%lu,", exe_ticks[i]);
+	}
+	printm("\n avg: %lu cycles\n", avg_ticks/16);
+
+	avg_ticks = 0;
+	printm("[PMPT-Micro#4 PMPT Alloc (unit: cycles)]: ");
+	for (int i=0; i<100; i++){
+		begin_tick = dd_get_ticks();
+		if (i<16)
+			pmpt_alloc_region(1, 256);
+		else
+			pmpt_alloc_region(0, 256);
+		end_tick = dd_get_ticks();
+		exe_ticks[i] = end_tick - begin_tick;
+		avg_ticks += exe_ticks[i];
+		printm("%lu,", exe_ticks[i]);
+	}
+	printm("\n avg: %lu cycles\n", avg_ticks/100);
+
+	avg_ticks = 0;
+	printm("[PMPT-Micro#5 PMP Free (unit: cycles)]: ");
+	for (int i=0; i<16; i++){
+		begin_tick = dd_get_ticks();
+		pmp_free_region();
+		end_tick = dd_get_ticks();
+		exe_ticks[i] = end_tick - begin_tick;
+		avg_ticks += exe_ticks[i];
+		printm("%lu,", exe_ticks[i]);
+	}
+	printm("\n avg: %lu cycles\n", avg_ticks/16);
+
+	avg_ticks = 0;
+	printm("[PMPT-Micro#6 PMPT Free (unit: cycles)]: ");
+	for (int i=0; i<100; i++){
+		begin_tick = dd_get_ticks();
+		if (i<16)
+			pmpt_free_region(1, 256);
+		else
+			pmpt_free_region(0, 256);
+		end_tick = dd_get_ticks();
+		exe_ticks[i] = end_tick - begin_tick;
+		avg_ticks += exe_ticks[i];
+		printm("%lu,", exe_ticks[i]);
+	}
+	printm("\n avg: %lu cycles\n", avg_ticks/100);
+
+	avg_ticks = 0;
+	printm("[PMPT-Micro#7 PMPT Alloc with differnent size (unit: cycles)]: ");
+	for (int i=0; i<6; i++){
+		begin_tick = dd_get_ticks();
+		pmpt_alloc_region(0, (1<<i)*1024);
+		end_tick = dd_get_ticks();
+		exe_ticks[i] = end_tick - begin_tick;
+		avg_ticks += exe_ticks[i];
+		printm("%lu,", exe_ticks[i]);
+	}
+	printm("\nend\n");
 }
