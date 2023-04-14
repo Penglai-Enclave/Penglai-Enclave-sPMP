2196513b36f366511c86c142925c5b724fafa9b1
Shang Qingyu <55442231+Shang-QY@users.noreply.github.com>
2023-04-14 23:16:54 +0800

    Penglai-v0.2 with OpenSBIv1.0(under dir opensbi-1.0) (clean version) (#86)
    * add opensbi-1.0 with penglai monitor in dir <opensbi-1.0>
    
    Signed-off-by: Qingyu Shang <2931013282@qq.com>
    
    * add build and run script for opensbi-1.0
    
    Signed-off-by: Qingyu Shang <2931013282@qq.com>
    
    ---------
    
    Signed-off-by: Qingyu Shang <2931013282@qq.com>

---
+++ opensbi-1.0/lib/sbi/objects.mk
@@ -0,0 +1,73 @@
+#
+# SPDX-License-Identifier: BSD-2-Clause
+#
+# Copyright (c) 2019 Western Digital Corporation or its affiliates.
+#
+# Authors:
+#   Anup Patel <anup.patel@wdc.com>
+#
+
+libsbi-objs-y += riscv_asm.o
+libsbi-objs-y += riscv_atomic.o
+libsbi-objs-y += riscv_hardfp.o
+libsbi-objs-y += riscv_locks.o
+
+libsbi-objs-y += sbi_bitmap.o
+libsbi-objs-y += sbi_bitops.o
+libsbi-objs-y += sbi_console.o
+libsbi-objs-y += sbi_domain.o
+libsbi-objs-y += sbi_ecall.o
+libsbi-objs-y += sbi_ecall_base.o
+libsbi-objs-y += sbi_ecall_hsm.o
+libsbi-objs-y += sbi_ecall_legacy.o
+libsbi-objs-y += sbi_ecall_pmu.o
+libsbi-objs-y += sbi_ecall_replace.o
+libsbi-objs-y += sbi_ecall_vendor.o
+libsbi-objs-y += sbi_emulate_csr.o
+libsbi-objs-y += sbi_fifo.o
+libsbi-objs-y += sbi_hart.o
+libsbi-objs-y += sbi_math.o
+libsbi-objs-y += sbi_hfence.o
+libsbi-objs-y += sbi_hsm.o
+libsbi-objs-y += sbi_illegal_insn.o
+libsbi-objs-y += sbi_init.o
+libsbi-objs-y += sbi_ipi.o
+libsbi-objs-y += sbi_misaligned_ldst.o
+libsbi-objs-y += sbi_platform.o
+libsbi-objs-y += sbi_pmu.o
+libsbi-objs-y += sbi_scratch.o
+libsbi-objs-y += sbi_string.o
+libsbi-objs-y += sbi_system.o
+libsbi-objs-y += sbi_timer.o
+libsbi-objs-y += sbi_tlb.o
+libsbi-objs-y += sbi_trap.o
+libsbi-objs-y += sbi_unpriv.o
+libsbi-objs-y += sbi_expected_trap.o
+libsbi-objs-y += sbi_pmp.o
+
+## Add by Dong Du
+#  The Penglai related files here
+libsbi-objs-y += sbi_ecall_penglai.o
+libsbi-objs-y += sm/enclave.o
+libsbi-objs-y += sm/pmp.o
+libsbi-objs-y += sm/sm.o
+libsbi-objs-y += sm/thread.o
+libsbi-objs-y += sm/utils.o
+libsbi-objs-y += sm/platform/pmp/platform.o
+libsbi-objs-y += sm/attest.o
+
+libsbi-objs-y += sm/gm/miracl/mrcore.o
+libsbi-objs-y += sm/gm/miracl/mrarth0.o
+libsbi-objs-y += sm/gm/miracl/mrarth1.o
+libsbi-objs-y += sm/gm/miracl/mrarth2.o
+libsbi-objs-y += sm/gm/miracl/mrcurve.o
+libsbi-objs-y += sm/gm/miracl/mrxgcd.o
+libsbi-objs-y += sm/gm/miracl/mrarth3.o
+libsbi-objs-y += sm/gm/miracl/mrjack.o
+libsbi-objs-y += sm/gm/miracl/mrbits.o
+libsbi-objs-y += sm/gm/miracl/mrmonty.o
+libsbi-objs-y += sm/gm/miracl/mrrand.o
+libsbi-objs-y += sm/gm/miracl/mrsroot.o
+libsbi-objs-y += sm/gm/miracl/mrlucas.o
+libsbi-objs-y += sm/gm/SM2_sv.o
+libsbi-objs-y += sm/gm/SM3.o
---
+++ opensbi-1.0/lib/sbi/sbi_ecall_penglai.c
@@ -0,0 +1,103 @@
+/*
+ * Authors:
+ *   Dong Du <Dd_nirvana@sjtu.edu.cn>
+ *   Erhu Feng <2748250768@qq.com>
+ */
+
+#include <sbi/sbi_ecall.h>
+#include <sbi/sbi_ecall_interface.h>
+#include <sbi/sbi_error.h>
+#include <sbi/sbi_trap.h>
+#include <sbi/sbi_version.h>
+#include <sbi/riscv_asm.h>
+#include <sbi/sbi_console.h>
+#include <sm/sm.h>
+
+
+static int sbi_ecall_penglai_host_handler(unsigned long extid, unsigned long funcid,
+		const struct sbi_trap_regs *regs, unsigned long *out_val,
+		struct sbi_trap_info *out_trap)
+{
+	uintptr_t ret = 0;
+
+	//csr_write(CSR_MEPC, regs->mepc + 4);
+	((struct sbi_trap_regs *)regs)->mepc += 4;
+
+	switch (funcid) {
+		// The following is the Penglai's Handler
+		case SBI_MM_INIT:
+			ret = sm_mm_init(regs->a0, regs->a1);
+			break;
+		case SBI_MEMORY_EXTEND:
+			ret = sm_mm_extend(regs->a0, regs->a1);
+			break;
+		case SBI_ALLOC_ENCLAVE_MM:
+			ret = sm_alloc_enclave_mem(regs->a0);
+			break;
+		case SBI_CREATE_ENCLAVE:
+			ret = sm_create_enclave(regs->a0);
+			break;
+		case SBI_RUN_ENCLAVE:
+			ret = sm_run_enclave((uintptr_t *)regs, regs->a0);
+			break;
+		case SBI_ATTEST_ENCLAVE:
+			ret = sm_attest_enclave(regs->a0, regs->a1, regs->a2);
+			break;
+		case SBI_STOP_ENCLAVE:
+			ret = sm_stop_enclave((uintptr_t *)regs, regs->a0);
+			break;
+		case SBI_RESUME_ENCLAVE:
+			ret = sm_resume_enclave((uintptr_t *)regs, regs->a0);
+			break;
+		case SBI_DESTROY_ENCLAVE:
+			ret = sm_destroy_enclave((uintptr_t *)regs, regs->a0);
+			break;
+		default:
+			sbi_printf("[Penglai@Monitor] host interface(funcid:%ld) not supported yet\n", funcid);
+			ret = SBI_ENOTSUPP;
+	}
+	//((struct sbi_trap_regs *)regs)->mepc = csr_read(CSR_MEPC);
+	//((struct sbi_trap_regs *)regs)->mstatus = csr_read(CSR_MSTATUS);
+	*out_val = ret;
+	return ret;
+}
+
+struct sbi_ecall_extension ecall_penglai_host = {
+	.extid_start = SBI_EXT_PENGLAI_HOST,
+	.extid_end = SBI_EXT_PENGLAI_HOST,
+	.handle = sbi_ecall_penglai_host_handler,
+};
+
+static int sbi_ecall_penglai_enclave_handler(unsigned long extid, unsigned long funcid,
+		const struct sbi_trap_regs *regs, unsigned long *out_val,
+		struct sbi_trap_info *out_trap)
+{
+	uintptr_t ret = 0;
+
+	//csr_write(CSR_MEPC, regs->mepc + 4);
+	((struct sbi_trap_regs *)regs)->mepc += 4;
+
+	switch (funcid) {
+		// The following is the Penglai's Handler
+		case SBI_EXIT_ENCLAVE:
+			ret = sm_exit_enclave((uintptr_t *)regs, regs->a0);
+			break;
+		case SBI_ENCLAVE_OCALL:
+			ret = sm_enclave_ocall((uintptr_t *)regs, regs->a0, regs->a1, regs->a2);
+			break;
+		case SBI_GET_KEY:
+			ret = sm_enclave_get_key((uintptr_t *)regs, regs->a0, regs->a1, regs->a2, regs->a3);
+			break;
+		default:
+			sbi_printf("[Penglai@Monitor] enclave interface(funcid:%ld) not supported yet\n", funcid);
+			ret = SBI_ENOTSUPP;
+	}
+	*out_val = ret;
+	return ret;
+}
+
+struct sbi_ecall_extension ecall_penglai_enclave = {
+	.extid_start = SBI_EXT_PENGLAI_ENCLAVE,
+	.extid_end = SBI_EXT_PENGLAI_ENCLAVE,
+	.handle = sbi_ecall_penglai_enclave_handler,
+};
