06ff232c412255c811ff6d6a9c7ceaf45df6b65b
pengxuanyao <pengxuanyao@foxmail.com>
2023-04-27 20:16:30 +0800

    fix bugs in enclave.c:  * add parameter definition for check_enclave_authentication  * add parameter declaration for platform_check_enclave_authentication

--- opensbi-1.0/include/sm/platform/spmp/platform_thread.h
+++ opensbi-1.0/include/sm/platform/spmp/platform_thread.h
@@ -9,7 +9,7 @@ void platform_exit_enclave_world();
 
 int platform_check_in_enclave_world();
 
-int platform_check_enclave_authentication();
+int platform_check_enclave_authentication(struct enclave_t* enclave);
 
 void platform_switch_to_enclave_ptbr(struct thread_state_t* thread, uintptr_t ptbr);
 
