177ccf7446303dc6d79f2f6d4b50bb87b8aa42c5
ZhaoXi <fly0307@sjtu.edu.cn>
2023-07-13 09:16:05 +0800

    🌈 style: No modify
    No code content has been modified

--- scripts/build_euler_kernel.sh
+++ scripts/build_euler_kernel.sh
@@ -4,6 +4,7 @@
 
 ## To create openEuler kernel config
 cd /home/penglai/penglai-enclave/openeuler-kernel
+# cd /home/zhaoxi/ipads/Penglai-Enclave-sPMP/openeuler-kernel
 cat openEuler_riscv64_defconfig >> arch/riscv/configs/defconfig
 make -s mrproper
 CROSS_COMPILE=riscv64-unknown-linux-gnu- make ARCH=riscv defconfig
@@ -11,6 +12,8 @@ CROSS_COMPILE=riscv64-unknown-linux-gnu- make ARCH=riscv defconfig
 
 ## To build kernel and enclave-driver
 cd /home/penglai/penglai-enclave/openeuler-kernel
+# cd /home/zhaoxi/ipads/Penglai-Enclave-sPMP/openeuler-kernel
 CROSS_COMPILE=riscv64-unknown-linux-gnu- make ARCH=riscv -j8
 cd /home/penglai/penglai-enclave/penglai-enclave-driver
+# cd /home/zhaoxi/ipads/Penglai-Enclave-sPMP/penglai-enclave-driver
 CROSS_COMPILE=riscv64-unknown-linux-gnu- make
