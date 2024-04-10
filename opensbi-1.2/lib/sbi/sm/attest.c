2bce73e1c9f56a2ffe5b1c2804c08388a797635d
Yilong Wang <2548340423@qq.com>
2024-04-10 19:39:46 +0800

    make hash_enclave the same as sign_tool
    Signed-off-by: Yilong Wang <2548340423@qq.com>

--- opensbi-1.2/lib/sbi/sm/attest.c
+++ opensbi-1.2/lib/sbi/sm/attest.c
@@ -79,14 +79,14 @@ void hash_enclave(struct enclave_t *enclave, void* hash, uintptr_t nonce_arg)
     SM3_process(&hash_ctx, (unsigned char*)(&(enclave->entry_point)),
         sizeof(unsigned long));
     // configuration parameters
-    SM3_process(&hash_ctx, (unsigned char*)(&(enclave->untrusted_ptr)),
-        sizeof(unsigned long));
-    SM3_process(&hash_ctx, (unsigned char*)(&(enclave->untrusted_size)),
-        sizeof(unsigned long));
-    SM3_process(&hash_ctx, (unsigned char*)(&(enclave->kbuffer)),
-        sizeof(unsigned long));
-    SM3_process(&hash_ctx, (unsigned char*)(&(enclave->kbuffer_size)),
-        sizeof(unsigned long));
+    // SM3_process(&hash_ctx, (unsigned char*)(&(enclave->untrusted_ptr)),
+    //     sizeof(unsigned long));
+    // SM3_process(&hash_ctx, (unsigned char*)(&(enclave->untrusted_size)),
+    //     sizeof(unsigned long));
+    // SM3_process(&hash_ctx, (unsigned char*)(&(enclave->kbuffer)),
+    //     sizeof(unsigned long));
+    // SM3_process(&hash_ctx, (unsigned char*)(&(enclave->kbuffer_size)),
+    //     sizeof(unsigned long));
     hash_enclave_mem(
         &hash_ctx,
         (pte_t*)(enclave->thread_context.encl_ptbr << RISCV_PGSHIFT),
